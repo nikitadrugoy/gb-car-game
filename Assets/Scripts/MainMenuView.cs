aa49378855012772fad202fca82137a00253a97b
nikitasidorin <nikitadrugoy@gmail.com>
2021-11-16 22:05:32 +0300

    first commit

---
+++ Assets/Scripts/MainMenuView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuView : MonoBehaviour
+{
+    [SerializeField] private Button _buttonStart;
+
+    public void Init(UnityAction startGame)
+    {
+        _buttonStart.onClick.AddListener(startGame);
+    }
+
+    protected void OnDestroy()
+    {
+        _buttonStart.onClick.RemoveAllListeners();
+    }
+}
